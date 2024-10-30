3bd869967aaec2d4919759c347fd655ae0d67c6c
sadeghb97 <sadeghb97@gmail.com>
2024-10-30 16:29:26 +0330

    Up LeageStandings

--- standings/LeagueStandings.php
+++ standings/LeagueStandings.php
@@ -13,6 +13,10 @@ class LeagueStandings {
         echo '<link href="https://fonts.googleapis.com/css?family=Fjalla+One" rel="stylesheet">';
     }
 
+    public function sort(){
+        usort($this->competitors, [$this, 'mainSort']);
+    }
+
     public function renderTable($title = null){
         echo '<div class="ptable">';
         if($title != null) echo '<h1 class="headin">' . $title . '</h1>';
@@ -25,22 +29,12 @@ class LeagueStandings {
                 <th>W</th>
                 <th>D</th>
                 <th>L</th>
+                <th>SC</th>
                 <th>GD</th>
                 <th>PTS</th>
                 </tr>';
 
-        usort($this->competitors, function (Competitor $a, Competitor $b){
-            if($a->points > $b->points) return -1;
-            else if($b->points > $a->points) return 1;
-
-            else if(($a->scores - $a->conceded) > ($b->scores - $b->conceded)) return -1;
-            else if(($b->scores - $b->conceded) > ($a->scores - $a->conceded)) return 1;
-
-            else if($a->scores > $b->scores) return -1;
-            else if($b->scores > $a->scores) return 1;
-
-            return 0;
-        });
+        $this->sort();
 
         foreach ($this->competitors as $competitorIndex => $competitor){
             $competitorRank = $competitorIndex + 1;
@@ -51,11 +45,25 @@ class LeagueStandings {
             echo '<th>' . $competitor->w . '</th>';
             echo '<th>' . $competitor->d . '</th>';
             echo '<th>' . $competitor->l . '</th>';
+            echo '<th>' . $competitor->scores . '</th>';
             echo '<th>' . $competitor->scores - $competitor->conceded . '</th>';
             echo '<th>' . $competitor->points . '</th>';
             echo '</tr>';
         }
 
         echo '</table></div>';
+    }
+
+    private function mainSort(Competitor $a, Competitor $b) : int {
+        if($a->points > $b->points) return -1;
+        else if($b->points > $a->points) return 1;
+
+        else if(($a->scores - $a->conceded) > ($b->scores - $b->conceded)) return -1;
+        else if(($b->scores - $b->conceded) > ($a->scores - $a->conceded)) return 1;
+
+        else if($a->scores > $b->scores) return -1;
+        else if($b->scores > $a->scores) return 1;
+
+        return 0;
     }
 }
