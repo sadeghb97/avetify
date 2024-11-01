c87669d1e4b956c10f6bcc06c994c001b7687950
sadeghb97 <sadeghb97@gmail.com>
2024-11-01 05:28:55 +0330

    up

--- entities/SortFactor.php
+++ entities/SortFactor.php
@@ -47,3 +47,27 @@ class SimpleTextSortFactor extends SimpleSortFactor {
         parent::__construct($title, $factorKey, $isDescending, false);
     }
 }
+
+class PipedSortFactors extends SortFactor {
+    /** @var SortFactor[] */
+    public array $sortFactors = [];
+
+    public function __construct(array $sortFactors){
+        $this->sortFactors = $sortFactors;
+        $mainFactor = $sortFactors[0];
+        parent::__construct($mainFactor->title, $mainFactor->factorKey,
+            $mainFactor->isDescending, $mainFactor->isNumeric);
+    }
+
+    public function getValue($item): float | string {
+        return $this->sortFactors[0]->getValue($item);
+    }
+
+    public function compare($itemA, $itemB): int {
+        foreach ($this->sortFactors as $sortFactor){
+            $comp = $sortFactor->compare($itemA, $itemB);
+            if($comp != 0) return $comp;
+        }
+        return 0;
+    }
+}
