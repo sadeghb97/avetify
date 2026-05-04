3612563440eea72a3ae53d60f781c02e30228618
Sadegh Bagherzadeh <sadeghb97@gmail.com>
2026-05-04 18:28:15 +0330

    adjust field containers

--- src/Entities/Fields/Containers/EntityFieldsContainer.php
+++ src/Entities/Fields/Containers/EntityFieldsContainer.php
@@ -2,8 +2,14 @@
 namespace Avetify\Entities\Fields\Containers;
 
 use Avetify\Entities\Fields\EntityFieldWrapper;
+use Avetify\Fields\BaseRecordField;
 
 /**
  * @method self modifyRecordSetSeparatorSize(int $sepSize)
  */
-class EntityFieldsContainer extends EntityFieldWrapper {}
+class EntityFieldsContainer extends EntityFieldWrapper {
+    public function __construct(BaseRecordField $recordField) {
+        parent::__construct($recordField);
+        $this->baseModifier?->pushStyle("margin", "auto");
+    }
+}
--- src/Table/Fields/Containers/TableFieldsContainer.php
+++ src/Table/Fields/Containers/TableFieldsContainer.php
@@ -1,9 +1,15 @@
 <?php
 namespace Avetify\Table\Fields\Containers;
 
+use Avetify\Fields\BaseRecordField;
 use Avetify\Table\Fields\TableFieldWrapper;
 
 /**
  * @method self modifyRecordSetSeparatorSize(int $sepSize)
  */
-class TableFieldsContainer extends TableFieldWrapper {}
+class TableFieldsContainer extends TableFieldWrapper {
+    public function __construct(BaseRecordField $recordField) {
+        parent::__construct($recordField);
+        $this->baseModifier?->pushStyle("margin", "auto");
+    }
+}
